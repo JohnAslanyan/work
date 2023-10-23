5fd8f7f9a55c7adedf53753690ab1ecdc3f8707b
John Udemy <148718047+UdemyJohn@users.noreply.github.com>
2023-10-23 13:35:54 -0700

    Add files via upload

---
+++ starter/assignments.js
@@ -0,0 +1,13 @@
+// let country = "Armenia";
+// let continent = "Europe";
+// let population = "3";
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 'jonas');
+// console.log(typeof 23);
+
+javascriptIsFun = "yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+year = 2024
+console.log(year);
